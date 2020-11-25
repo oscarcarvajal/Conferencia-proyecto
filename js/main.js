8dcdc870de479b0df17f9478d0eab599d37ecdbb
Carvajal <oscarcarcarvajal@outlook.es>
2020-11-24 19:31:18 -0800

    Modify talleres with jquery

--- js/main.js
+++ js/main.js
@@ -136,3 +136,19 @@
 
     }); //DOM content Loaded
 })();
+
+$(function() {
+
+    //Programa de conferencias
+    $('.programa-evento .info-curso:first').show();
+    $('.menu-programa a:first').addClass('activo');
+    $('.menu-programa a').on('click', function() {
+        $('.menu-programa a').removeClass('activo');
+        $(this).addClass('activo');
+        $('.ocultar').hide();
+        enlace = $(this).attr('href');
+        $(enlace).fadeIn(1000);
+
+        return false;
+    })
+});
