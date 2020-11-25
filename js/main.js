b595cff7f7428099b6ce42430201c0e44fdd7f4f
Carvajal <oscarcarcarvajal@outlook.es>
2020-11-24 20:07:09 -0800

    Add pluggins for time and guest

--- js/main.js
+++ js/main.js
@@ -150,5 +150,18 @@ $(function() {
         $(enlace).fadeIn(1000);
 
         return false;
+    });
+    //Animaciones para los Numeros
+    $('.resumen-evento li:nth-child(1) p').animateNumber({ number: 6 }, 1200);
+    $('.resumen-evento li:nth-child(2) p').animateNumber({ number: 15 }, 1200);
+    $('.resumen-evento li:nth-child(3) p').animateNumber({ number: 3 }, 1500);
+    $('.resumen-evento li:nth-child(4) p').animateNumber({ number: 9 }, 1200);
+
+    //Cuenta regresiva
+    $('.cuenta-regresiva').countdown('2020/12/10 09:00:00', function(event) {
+        $('#dias').html(event.strftime('%D'));
+        $('#horas').html(event.strftime('%H'));
+        $('#minutos').html(event.strftime('%M'));
+        $('#segundos').html(event.strftime('%S'));
     })
 });
