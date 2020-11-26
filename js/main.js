574769cb0bf95a84f974f635a52c100c34d1717f
Carvajal <oscarcarcarvajal@outlook.es>
2020-11-25 17:36:11 -0800

    Add pluggins to make the page responsive

--- js/main.js
+++ js/main.js
@@ -1,18 +1,9 @@
 (function() {
     "use strict";
+
     var regalo = document.getElementById('regalo');
     document.addEventListener('DOMContentLoaded', function() {
-        var map = L.map('mapa').setView([33.795733, -117.897248], 16);
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
-
-        L.marker([33.795733, -117.897248]).addTo(map)
-            .bindPopup('ProyectoGDL 2020, <br> Boletos ya disponibles')
-            .openPopup()
-            .bindTooltip('un Tooltip')
-            .openTooltip();
         //Campos Datos usuarios
         var nombre = document.getElementById('nombre');
         var apellido = document.getElementById('apellido');
@@ -35,109 +26,148 @@
         var etiquetas = document.getElementById('etiquetas');
 
 
-        calcular.addEventListener('click', calcularMontos);
-
-        pase_dia.addEventListener('blur', mostrarDias);
-        pase_dosdias.addEventListener('blur', mostrarDias);
-        pase_completo.addEventListener('blur', mostrarDias);
-
-        nombre.addEventListener('blur', validarCampos);
-        apellido.addEventListener('blur', validarCampos);
-        email.addEventListener('blur', validarCampos);
-        email.addEventListener('blur', validarMail);
+        if (document.getElementById('calcular')) {
 
 
-        function validarCampos() {
-            if (this.value == '') {
-                errorDiv.style.display = 'block';
-                errorDiv.innerHTML = "Este campo es obligatorio";
-                this.style.border = '1px solid red';
-                errorDiv.style.border = '1px solid red';
-            } else {
-                errorDiv.style.display = 'none';
-                this.style.border = '1px solid #cccccc';
+            calcular.addEventListener('click', calcularMontos);
+
+            pase_dia.addEventListener('blur', mostrarDias);
+            pase_dosdias.addEventListener('blur', mostrarDias);
+            pase_completo.addEventListener('blur', mostrarDias);
+
+            nombre.addEventListener('blur', validarCampos);
+            apellido.addEventListener('blur', validarCampos);
+            email.addEventListener('blur', validarCampos);
+            email.addEventListener('blur', validarMail);
+
+
+            function validarCampos() {
+                if (this.value == '') {
+                    errorDiv.style.display = 'block';
+                    errorDiv.innerHTML = "Este campo es obligatorio";
+                    this.style.border = '1px solid red';
+                    errorDiv.style.border = '1px solid red';
+                } else {
+                    errorDiv.style.display = 'none';
+                    this.style.border = '1px solid #cccccc';
+                }
             }
-        }
 
-        function validarMail() {
-            if (this.value.indexOf("@") > -1) {
-                errorDiv.style.display = 'none';
-                this.style.border = '1px solid #cccccc';
-            } else {
-                errorDiv.style.display = 'block';
-                errorDiv.innerHTML = "Debe tener al menos una @";
-                this.style.border = '1px solid red';
-                errorDiv.style.border = '1px solid red';
+            function validarMail() {
+                if (this.value.indexOf("@") > -1) {
+                    errorDiv.style.display = 'none';
+                    this.style.border = '1px solid #cccccc';
+                } else {
+                    errorDiv.style.display = 'block';
+                    errorDiv.innerHTML = "Debe tener al menos una @";
+                    this.style.border = '1px solid red';
+                    errorDiv.style.border = '1px solid red';
+                }
             }
-        }
 
-        function calcularMontos(event) {
-            event.preventDefault();
-            if (regalo.value === '') {
-                alert("Debes elegir un regalo");
-                regalo.focus();
-            } else {
+            function calcularMontos(event) {
+                event.preventDefault();
+                if (regalo.value === '') {
+                    alert("Debes elegir un regalo");
+                    regalo.focus();
+                } else {
+                    var boletosDia = parseInt(pase_dia.value, 10) || 0,
+                        boletos2dias = parseInt(pase_dosdias.value, 10) || 0,
+                        boletosCompleto = parseInt(pase_completo.value, 10) || 0,
+                        cantCamisas = parseInt(camisas.value, 10) || 0,
+                        cantEtiquetas = parseInt(etiquetas.value, 10) || 0;
+
+                    var totalPagar = (boletosDia * 30) + (boletos2dias * 45) + (boletosCompleto * 50) + ((cantCamisas * 10) * .93) + (cantEtiquetas * 2);
+
+                    var listadoProductos = [];
+
+                    if (boletosDia >= 1) {
+                        listadoProductos.push(boletosDia + ' Pases por dia');
+                    }
+                    if (boletos2dias >= 1) {
+                        listadoProductos.push(boletos2dias + ' Pases por 2 dias');
+                    }
+                    if (boletosCompleto >= 1) {
+                        listadoProductos.push(boletosCompleto + ' Pases Completos');
+                    }
+                    if (cantCamisas >= 1) {
+                        listadoProductos.push(cantCamisas + ' Camisas');
+                    }
+                    if (cantEtiquetas >= 1) {
+                        listadoProductos.push(cantEtiquetas + ' Etiquetas');
+                    }
+                    lista_productos.style.display = "block";
+                    lista_productos.innerHTML = '';
+                    for (var i = 0; i < listadoProductos.length; i++) {
+                        lista_productos.innerHTML += listadoProductos[i] + '<br>';
+                    }
+                    suma.innerHTML = "$" + totalPagar.toFixed(2);
+
+                }
+            }
+
+            function mostrarDias() {
                 var boletosDia = parseInt(pase_dia.value, 10) || 0,
                     boletos2dias = parseInt(pase_dosdias.value, 10) || 0,
-                    boletosCompleto = parseInt(pase_completo.value, 10) || 0,
-                    cantCamisas = parseInt(camisas.value, 10) || 0,
-                    cantEtiquetas = parseInt(etiquetas.value, 10) || 0;
+                    boletosCompleto = parseInt(pase_completo.value, 10) || 0;
 
-                var totalPagar = (boletosDia * 30) + (boletos2dias * 45) + (boletosCompleto * 50) + ((cantCamisas * 10) * .93) + (cantEtiquetas * 2);
+                var diasElegidos = [];
 
-                var listadoProductos = [];
-
-                if (boletosDia >= 1) {
-                    listadoProductos.push(boletosDia + ' Pases por dia');
+                if (boletosDia > 0) {
+                    diasElegidos.push('viernes');
                 }
-                if (boletos2dias >= 1) {
-                    listadoProductos.push(boletos2dias + ' Pases por 2 dias');
+                if (boletos2dias > 0) {
+                    diasElegidos.push('viernes', 'sabado');
                 }
-                if (boletosCompleto >= 1) {
-                    listadoProductos.push(boletosCompleto + ' Pases Completos');
+                if (boletosCompleto > 0) {
+                    diasElegidos.push('viernes', 'sabado', 'domingo');
                 }
-                if (cantCamisas >= 1) {
-                    listadoProductos.push(cantCamisas + ' Camisas');
+                for (var i = 0; diasElegidos.length; i++) {
+                    document.getElementById(diasElegidos[i]).style.display = 'block';
                 }
-                if (cantEtiquetas >= 1) {
-                    listadoProductos.push(cantEtiquetas + ' Etiquetas');
-                }
-                lista_productos.style.display = "block";
-                lista_productos.innerHTML = '';
-                for (var i = 0; i < listadoProductos.length; i++) {
-                    lista_productos.innerHTML += listadoProductos[i] + '<br>';
-                }
-                suma.innerHTML = "$" + totalPagar.toFixed(2);
-
             }
+
         }
+        var map = L.map('mapa').setView([33.795733, -117.897248], 16);
 
-        function mostrarDias() {
-            var boletosDia = parseInt(pase_dia.value, 10) || 0,
-                boletos2dias = parseInt(pase_dosdias.value, 10) || 0,
-                boletosCompleto = parseInt(pase_completo.value, 10) || 0;
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
 
-            var diasElegidos = [];
-
-            if (boletosDia > 0) {
-                diasElegidos.push('viernes');
-            }
-            if (boletos2dias > 0) {
-                diasElegidos.push('viernes', 'sabado');
-            }
-            if (boletosCompleto > 0) {
-                diasElegidos.push('viernes', 'sabado', 'domingo');
-            }
-            for (var i = 0; diasElegidos.length; i++) {
-                document.getElementById(diasElegidos[i]).style.display = 'block';
-            }
-        }
-
+        L.marker([33.795733, -117.897248]).addTo(map)
+            .bindPopup('ProyectoGDL 2020, <br> Boletos ya disponibles')
 
     }); //DOM content Loaded
 })();
 
 $(function() {
+
+    //lettering
+
+    $('.nombre-sitio').lettering();
+
+    //Menu fijo
+    var windowHeight = $(window).height();
+    var barraAltura = $('.barra').innerHeight();
+
+
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll > windowHeight) {
+            $('.barra').addClass('fixed');
+            $('body').css({ 'margin-top': barraAltura + 'px' });
+        } else {
+            $('.barra').removeClass('fixed');
+            $('body').css({ 'margin-top': '0px' });
+        }
+
+    });
+
+
+    //Menu Responsive
+    $('.menu-movil').on('click', function() {
+        $('.navegacion-principal').slideToggle();
+    });
 
     //Programa de conferencias
     $('.programa-evento .info-curso:first').show();
